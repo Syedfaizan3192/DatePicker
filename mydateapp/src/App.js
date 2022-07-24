cd6cea8d551a8cf1f57d4f17cd03b0e30f4e2b91
Syedfaizan3192 <syedfaizan3192@gmail.com>
2022-07-24 04:00:58 -0700

    Datepicker

---
+++ mydateapp/src/App.js
@@ -0,0 +1,24 @@
+import CalendarComp from './components/CalendarComp.jsx'
+import DateRangeComp from './components/DateRangeComp.jsx'
+import DateRangePickerComp from './components/DateRangePickerComp.jsx'
+
+import './App.css'
+
+const App = () => {
+  return (
+    <div className="App">
+
+      <br /><br />
+      <hr />
+
+      <h4>React Date Range Select</h4>
+      <DateRangeComp />
+
+      <br /><br />
+      <hr />
+
+    </div>
+  );
+}
+
+export default App
